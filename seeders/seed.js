cc5f94069e349dae65f4cc56a9e0f0da6b189469
James Botham <jamesbotham@gmail.com>
2020-01-30 16:46:42 -0600

    testing heroku seed

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,10 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 let workoutSeed = [
   {
@@ -135,13 +135,16 @@ let workoutSeed = [
   }
 ];
 
+console.log(JSON.stringify(workoutSeed))
 db.Workout.deleteMany({})
   .then(() => db.Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
-    process.exit(0);
+    // process.exit(0);
   })
   .catch(err => {
     console.error(err);
-    process.exit(1);
+    // process.exit(1);
   });
+
+
