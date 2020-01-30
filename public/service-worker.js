a4f0ed8978d4749707af1fded8814b6b31151ae4
James Botham <jamesbotham@gmail.com>
2020-01-29 18:30:40 -0600

    added pwa functionallity still needs to fix install path

---
+++ public/service-worker.js
@@ -0,0 +1,88 @@
+
+let FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/api.js",
+  "/exercise.html",
+  "/exercise.js",
+  "/index.js",
+  "/stats.html",
+  "/stats.js",
+  "/style.css",
+  "/workout-style.css",
+  "/workout.js",
+  "https://use.fontawesome.com/releases/v5.8.2/css/all.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/materia/bootstrap.min.css"
+
+];
+const CACHE_NAME = 'static-cache-v3';
+
+const DATA_CACHE_NAME = 'data-cache-4';
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    );
+    self.skipWaiting();
+});
+
+
+self.addEventListener("activate", (evt) => {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data", key');
+                        return caches.delete(key);
+                    }
+                })
+
+            )
+            .catch(err => {
+              console.log(err)
+            })
+        })
+    )
+    self.clients.claim();
+})
+
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => {
+          console.log(err)
+        })
+      )
+      return;
+    }
+
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  });
+  
+  
