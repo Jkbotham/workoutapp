e5e1d2d83742660e5f77fa2ddae2176d62701900
James Botham <jamesbotham@gmail.com>
2020-01-30 17:56:05 -0600

    Reverting files

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,10 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 let workoutSeed = [
   {
@@ -140,11 +140,11 @@ db.Workout.deleteMany({})
   .then(() => db.Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
-    // process.exit(0);
+    process.exit(0);
   })
   .catch(err => {
     console.error(err);
-    // process.exit(1);
+    process.exit(1);
   });
 
 
