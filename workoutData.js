87f8e173941ecc7dd76013cd56df5f2286a6e4eb
James <jamesbotham@gmail.com>
2020-04-18 11:12:04 -0500

    updated workout data

--- workoutData.js
+++ workoutData.js
@@ -304,3 +304,240 @@ const dayThreeShoulders = [
 }
 
 ]
+
+// -----------------------------
+
+const dayFourChest = [
+    {
+        exerciseName: "Incline Dumbbell Bench Press",
+        sets: [12, 10, 10, 8],
+        actualSets: [
+            {
+                reps: 12,
+                weight: 25
+            },
+            {
+                reps: 10,
+                weight: 30
+            },
+            {
+                reps: 10,
+                weight: 30
+            },
+            {
+                reps: 8,
+                weight: 35
+            },
+        ]
+    },
+    {
+        exerciseName: "Dubbell Bench Press",
+        sets: [12, 10, 10, 8],
+        actualSets: [
+            {
+                reps: 12,
+                weight: 35
+            },
+            {
+                reps: 10,
+                weight: 40
+            },
+            {
+                reps: 10,
+                weight: 45
+            },
+            {
+                reps: 8,
+                weight: 50
+            },
+        ]
+    },
+    {
+        exerciseName: "Incline Dumbbell Fly",
+        sets: [12, 12, 12],
+        actualSets: [
+            {
+                reps: 12,
+                weight: 15
+            },
+            {
+                reps: 12,
+                weight: 15
+            },
+            {
+                reps: 12,
+                weight: 15
+            }
+        ]
+    },
+    {
+        exerciseName: "Dumbbell Skull Crusher",
+        sets: [12, 12, 12],
+        actualSets: [
+            {
+                reps: 12,
+                weight: 15
+            },
+            {
+                reps: 12,
+                weight: 15
+            },
+            {
+                reps: 12,
+                weight: 15
+            }
+        ]
+    },
+    {
+        exerciseName: "Dumbbell Close Grip Bench Press",
+        sets: [8, 8, 8],
+        actualSets: [
+            {
+                reps: 8,
+                weight: 25
+            },
+            {
+                reps: 8,
+                weight: 25
+            },
+            {
+                reps: 8,
+                weight: 25
+            }
+        ]
+    },
+    {
+        exerciseName: "One Arm Dumbbell Extension",
+        sets: [12, 12, 12],
+        actualSets: [
+            {
+                reps: 12,
+                weight: 10
+            },
+            {
+                reps: 12,
+                weight: 10
+            }, {
+                reps: 12,
+                weight: 10
+            }
+        ]
+    }
+]
+
+
+const backWorkout = [
+    {
+        exerciseName: "One Arm Dumbbell Row",
+        sets: [12, 10, 10, 8, 6],
+        actualSets: [
+            {
+                reps: 10,
+                weight: 35
+            },
+            {
+                reps: 10,
+                weight: 35
+            },            {
+                reps: 10,
+                weight: 35
+            },            {
+                reps: 10,
+                weight: 35
+            }
+        ]
+    },
+    {
+        exerciseName: "Bent Over Dumbbell Row",
+        sets: [12, 10, 10, 8, 6],
+        actualSets: [
+            {
+                reps: 10,
+                weight: 40
+            },
+            {
+                reps: 10,
+                weight: 40
+            },
+            {
+                reps: 10,
+                weight: 40
+            },
+            {
+                reps: 10,
+                weight: 40
+            }
+        ]
+    },
+    {
+        exerciseName: "Dumbbell Pullover",
+        sets: [10, 10],
+        actualSets: [
+            {
+                reps: 10,
+                weight: 20
+            },
+            {
+                reps: 10,
+                weight: 20
+            }
+        ],
+        comments: "Add 5 lb next week"
+    },
+    {
+        exerciseName: "Incline Dumbbel Curl",
+        sets: [10, 10, 10],
+        actualSets: [
+            {
+                reps: 10,
+                weight: 15
+            },
+            {
+                reps: 10,
+                weight: 15
+            },
+            {
+                reps: 10,
+                weight: 15
+            }
+        ]
+    },
+    {
+        exerciseName: "Standing Dumbbel Curl",
+        sets: [10, 10, 10],
+        actualSets: [
+            {
+                reps: 10,
+                weight: 15
+            },
+            {
+                reps: 10,
+                weight: 15
+            },
+            {
+                reps: 10,
+                weight: 15
+            }
+        ]
+    },
+    {
+        exerciseName: "Cross Body Hammer Curl",
+        sets: [10, 10, 10],
+        actualSets: [
+            {
+                reps: 10,
+                weight: 15
+            },
+            {
+                reps: 10,
+                weight: 15
+            },
+            {
+                reps: 8,
+                weight: 15
+            }
+        ],
+        comments: "15 was surprisingly difficult"
+    },
+
+];
